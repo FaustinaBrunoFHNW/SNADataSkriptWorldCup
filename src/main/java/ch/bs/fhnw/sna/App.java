f85d6331ec921cdfb95dce8d1e49a771a1539a78
FaustinaBrunoFHNW <faustina.bruno@students.fhnw.ch>
2019-12-21 13:01:02 +0100

    File einlesen

--- src/main/java/ch/bs/fhnw/sna/App.java
+++ src/main/java/ch/bs/fhnw/sna/App.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class App {
 
-
     public static void main(String[] args) {
+        Reader reader = new Reader();
 
-
+        reader.einlesen();
 
     }
 }
--- src/main/java/ch/bs/fhnw/sna/Reader.java
+++ src/main/java/ch/bs/fhnw/sna/Reader.java
@@ -11,8 +11,49 @@ import java.util.logging.Level;
 //TODO File einlesen
 public class Reader {
 
+
+    private static final String PATHNAME_DATASET = "src/main/java/resources/rawDataset.csv";
+
     //TODO File einlesen
-    public static void einlesen(){}
+    public static void einlesen(){
+
+        FileReader csvFile = null;
+        try {
+            csvFile = new FileReader("src/main/resources/rawDataset.csv");
+        } catch (FileNotFoundException e) {
+
+            e.printStackTrace();
+        }
+
+        try {
+            BufferedReader br;
+            //   System.out.println(spamFile.getAbsolutePath().toString()+ " File wird geprüft");
+            br = new BufferedReader(csvFile);
+            String linie = br.readLine();
+          //  System.out.println(linie.toString() + " diese Linie wird geprüft");
+
+            while (linie != null) {
+             //   this.wortListeInEmailBefuellen(linie, email);
+                linie = br.readLine();
+            }
+
+        //    System.out.println("Diese Mail hat " + email.getWoerter().size() + "  Woerter");
+
+        //    spamMails.add(email);
+
+            br.close();
+
+        } catch (FileNotFoundException e) {
+
+            System.out.println("Beim einlesen ist was schief gelaufen");
+            e.printStackTrace();
+
+        } catch (IOException e) {
+            System.out.println("Beim einlesen ist was schief gelaufen");
+            e.printStackTrace();
+        }
+
+    }
 
 
     //TODO in File schreiben
